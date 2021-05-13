e33513d64f63426ae9c6900b6db5a2c18da688d3
Felipe <felipeemersonsb@gmail.com>
2021-05-13 16:39:41 -0300

    npm demo

---
+++ index.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+//require - ordem de busca pelo módulo
+//1. core module
+//2. arquivo ou pasta
+//3. node_modules
+
+result = _.contains([1, 2, 3], 2);
+
+console.log(result);
